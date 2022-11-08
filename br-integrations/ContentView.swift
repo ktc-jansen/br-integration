b24ed7c10a120c20daaead2d0ece3492d10e3553
Willy Jansen <willyjansen@protonmail.com>
2022-11-08 11:03:32 +0900

    Initial Commit

---
+++ br-integrations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  br-integrations
+//
+//  Created by ヤンセン ウィリー（Willy Jansen） on 2022/11/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ br-integrations/br_integrationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  br_integrationsApp.swift
+//  br-integrations
+//
+//  Created by ヤンセン ウィリー（Willy Jansen） on 2022/11/08.
+//
+
+import SwiftUI
+
+@main
+struct br_integrationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
